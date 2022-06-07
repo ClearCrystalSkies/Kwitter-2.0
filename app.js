2bf505fa50788183acda1d3640ca862b6dfa792e
ClearCrystalSkies <100354673+ClearCrystalSkies@users.noreply.github.com>
2022-06-07 21:14:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function allNew(){
+    window.location = "index.html";
+}
+function Super(){
+    window.location = "super.html";
+}
+function triology(){
+    window.location= "triology.html";
+}
+function mystery(){
+    window.location = "mystery.html";
+}
---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7I08K2qADN6RZWPdXx_ooGY2BbPlQjTg",
+    authDomain: "kwitter2-d65c9.firebaseapp.com",
+    projectId: "kwitter2-d65c9",
+    storageBucket: "kwitter2-d65c9.appspot.com",
+    messagingSenderId: "955404575403",
+    appId: "1:955404575403:web:69a78d8d3010989350cdee"
+  };
+  userName = localStorage.getItem("User Name");
+document.getElementById("username_holder").innerHTML = userName;
